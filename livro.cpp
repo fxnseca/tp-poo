2b464ec8417a5003320911d88c613e9d5a7b0d79
fxnseca <fonsecalavinia25@gmail.com>
2022-09-14 20:40:44 -0300

    atualização teste

--- livro.cpp
+++ livro.cpp
@@ -7,11 +7,11 @@ typedef const char* String;
 
 //Construtor
 Livro::Livro(vector<String> esc, vector<String> tit, vector<String> cap,
- int anoPub, String idi, vector<String> keyw){
+ int anoPub, String idi, vector<String> key){
     escritores: esc;
     titulo: tit;
     capitulos: cap;
     anoPublicacao: anoPub;
     idiomaOriginal: idi;
-    keywords: keyw;
+    keywords: key;
 }
